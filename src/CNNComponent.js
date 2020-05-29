7de2cc8b7219370a4c7f74e85b9d6f01397a20c2
Jeet Mishra <jeet.jm68@gmail.com>
2020-05-29 15:09:13 +0530

    Initial Commit

---
+++ src/CNNComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import "./cnn_style.css"
+export default class CNNComponent extends Component {
+    render() {
+        return (
+            
+                <iframe src="https://jeet183.github.io/CNN-Digit-Recognition-Demo/"
+            frameborder="0" 
+            marginheight="0" 
+            marginwidth="0" 
+            width="100%" 
+            height="100%" 
+            scrolling="auto"> 
+            </iframe> 
+           
+        )
+    }
+}
